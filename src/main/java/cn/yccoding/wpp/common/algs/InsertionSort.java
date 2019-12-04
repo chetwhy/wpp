1dc379ea629136c7cc5a2893741d9948a8a99895
chetwhy <691409320@qq.com>
2019-12-04 22:54:26 +0800

    Update InsertionSort.java

--- src/main/java/cn/yccoding/wpp/common/algs/InsertionSort.java
+++ src/main/java/cn/yccoding/wpp/common/algs/InsertionSort.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
 public class InsertionSort {
 
     public static void sort(Comparable[] a) {
-        for (int i = 0; i < a.length; i++) {
+        for (int i = 1; i < a.length; i++) {
             for (int j = i; j > 0 && less(a[j], a[j - 1]); j--) {
                 exch(a, j, j - 1);
             }
